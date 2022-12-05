d1ad0ab439e9e17b5a8060052e3de38bc1a9f4e6
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-12-05 12:27:24 -0300

    feature: creating entities

---
+++ Store.Domain/Entities/Order.cs
@@ -0,0 +1,47 @@
+﻿using Store.Domain.Enums;
+
+namespace Store.Domain.Entities;
+
+public class Order : Entity
+{
+    public Order(Customer customer, decimal deliveryFee, Discount discount)
+    {
+        Customer = customer;
+        Date = DateTime.Now;
+        Number = Guid.NewGuid().ToString().Substring(0, 8);
+        Items = new List<OrderItem>();
+        DeliveryFee = deliveryFee;
+        Discount = discount;
+        Status = EOrderStatus.WaitingPayment;
+    }
+
+    public Customer Customer { get; private set; }
+    public DateTime Date { get; private set; }
+    public string Number { get; private set; }
+    public IList<OrderItem> Items { get; private set; }
+    public decimal DeliveryFee { get; private set; }
+    public Discount Discount { get; private set; }
+    public EOrderStatus Status { get; private set; }
+
+    public void AddItem(Product product, int quantity)
+    {
+        var item = new OrderItem(product, quantity);
+        Items.Add(item);
+    }
+
+    public decimal Total()
+    {
+        var total = Items.Sum(item => item.Total());
+
+        total += DeliveryFee;
+        total -= Discount != null ? Discount.Value() : 0;
+
+        return total;
+    }
+
+    public void Pay(decimal amount)
+    {
+        if (amount == Total())
+            this.Status = EOrderStatus.WaitingDelivery;
+    }
+}
