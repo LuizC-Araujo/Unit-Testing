44761dd91a948eee7da562ff60e78bdd753a1378
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-12-07 14:25:27 -0300

    feature: order tests red

---
+++ Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Entities;
+using Store.Domain.Enums;
+
+namespace Store.Tests.Entities;
+
+[TestClass]
+public class OrderTests
+{
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Generate_Number_With_8_Caractereres_When_Valid_Order()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Change_Status_To_Waiting_Payment_When_New_Order()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Change_Status_To_Waiting_Delivery_When_Order_Payment_Done()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Change_Status_To_Cancelled_When_Cancelled_Order()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Not_Add_New_Item_When_No_Product()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Not_Add_New_Item_When_Product_Less_Or_Equal_Zero()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Total_Be_50_When_New_Order_Valid()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Total_Be_60_When_Expired_Discount()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Total_Be_60_When_Invalid_Discount()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Total_Be_50_When_Discount_10()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Total_Be_60_When_Delivery_Fee_10()
+    {
+        Assert.Fail();
+    }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Return_Invalid_When_No_Customer()
+    {
+        Assert.Fail();
+    }
+}
