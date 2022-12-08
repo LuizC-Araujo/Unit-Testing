9ba0ffc4bc7b669eda4eb4834b1032b2969f0baa
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-12-08 12:17:57 -0300

    feature: tests order items red

---
+++ Store.Tests/Entities/OrderItemTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Store.Tests.Entities;
+
+[TestClass]
+public class OrderItemTests
+{
+
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Return_Invalid_When_Quantity_Zero()
+    {
+        Assert.Fail();
+    }
+}
