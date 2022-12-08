164e584b50b7c3206a523aa96a032c6fd4058219
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-12-08 12:25:33 -0300

    feature: test Should_Return_Valid_When_Order_Item_Valid

--- Store.Tests/Entities/OrderItemTests.cs
+++ Store.Tests/Entities/OrderItemTests.cs
@@ -6,13 +6,21 @@ namespace Store.Tests.Entities;
 [TestClass]
 public class OrderItemTests
 {
+    private readonly Product _product = new Product("Produto 1", 25, true);
 
     [TestMethod]
     [TestCategory("Domain")]
     public void Should_Return_Invalid_When_Quantity_Zero()
     {
-        var product = new Product("Produto 1", 25, true);
-        var orderItem = new OrderItem(product, 0);
+        var orderItem = new OrderItem(_product, 0);
         Assert.AreEqual(false, orderItem.IsValid);
+    }
+
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Should_Return_Valid_When_Order_Item_Valid()
+    {
+        var orderItem = new OrderItem(_product, 15);
+        Assert.AreEqual(true, orderItem.IsValid);
     }
 }
